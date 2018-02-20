44c6ea5d482f4c60a7ab4643a4d3338511f164bc
alexpatde <alexpatde@gmail.com>
2018-02-20 16:20:51 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<style>
+section { 
+    display: block;
+    border: 1px solid;
+    text-decoration-color:#ba8225 ;
+}
+</style>
+</head>
+<header style="background-color: #753006; border: 1px solid; width: 100%; height: 20%">
+CASITA DE LA ABUELA
+</header>
+<body>
+
+
+<section style="background-color:#ba8225;">
+<p >HOLA MUNDO</p>
+
+</section>
+</body>
+</html>
+
+
+<?php
+
+
+
+?>
